3b40efc43f6b8ab9e99247d120671d425560d9ed
ugokingsley <ugokingsley5@gmail.com>
2022-10-17 17:50:31 +0100

    add screenshots as project description

--- nextjsfrontend/.next/static/chunks/webpack.js
+++ nextjsfrontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "529fbf442166ea38"; }
+/******/ 		__webpack_require__.h = function() { return "986b7192d904fd09"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- nextjsfrontend/.next/static/webpack/webpack.fa61458155515cb5.hot-update.js
+++ nextjsfrontend/.next/static/webpack/webpack.fa61458155515cb5.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "529fbf442166ea38"; }
+/******/ 	__webpack_require__.h = function() { return "475df49bf8b15a09"; }
 /******/ }();
 /******/ 
 /******/ }
